240f8e1dacc75c618bde1157beea9c800926f0cf
Lusiolla <lusiolla@yandex.ru>
2021-12-01 18:26:41 +0300

    Объявлен метод main!

--- Praktikum.java
+++ Praktikum.java
@@ -1,8 +1,4 @@
 public class Praktikum {
     public static void main(String[] args) {
-        HashMap<String, String> officeTool = new HashMap<>();
-        officeTool.put("S234", "Большой степлер");
-        officeTool.put("P342", "Чёрно-белый принтер из коридора");
-        officeTool.put("N845", "Острые ножницы");
-        System.out.println(officeTool);
+
     }
